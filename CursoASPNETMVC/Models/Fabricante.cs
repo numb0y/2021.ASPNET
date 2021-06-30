d2f156ce11521773a763d5c192c867d1ff6d0409
Lucas Kramer <lucasPatriota@outlook.com>
2021-06-29 21:37:30 -0300

    inserção

---
+++ CursoASPNETMVC/Models/Fabricante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoASPNETMVC.Models
+{
+    public class Fabricante
+    {
+        public long FabricanteId { get; set; }
+        public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
+    }
+}
